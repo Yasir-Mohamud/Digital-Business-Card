d115550b2bf93b98e0825847d9fcc6ca8b898aee
Yasir-Mohamud <ymohamed018@gmail.com>
2021-11-30 20:07:49 -0800

    adds info component

---
+++ src/components/Info.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Info() {
+  return (
+    <div>
+      <img src="https://via.placeholder.com/271" alt="" />
+      <h2> Yasir Mohamud </h2>
+      <h4> Software Developer </h4>
+      <h5> Email : ymohamed018@gmail.com </h5>
+      <button>
+        <a href="www.linkedin.com/in/yasirmohamud"> LinkedIn </a>
+      </button>
+    </div>
+  );
+}
---
+++ src/components/Intrests.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Intrests() {
+  return <h1> Intrests</h1>;
+}
